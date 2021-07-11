b06579b36c0f124cdc6d2b47581a2428cc4d1484
zhongpeng <859247268@qq.com>
2021-07-11 13:53:00 +0800

    feat: 项目引入antd-mobile

---
+++ src/views/Interaction/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Interaction extends Component {
+  render() {
+    return <div> Interaction </div>;
+  }
+}
+
+export default Interaction;
--- src/views/Layout/index.jsx
+++ src/views/Layout/index.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import { Button } from "antd-mobile";
 
 class Layout extends Component {
   render() {
-    return <div> Layout </div>;
+    return (
+      <div>
+        <Button type="primary" size="small" inline>small</Button>
+      </div>
+    );
   }
 }
 
