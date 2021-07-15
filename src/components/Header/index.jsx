640e26bd40f10015ebdb2eb30d516007ee19a1d8
zhongpeng <859247268@qq.com>
2021-07-15 22:19:55 +0800

    feat: 我的页面布局完成

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const style = {
+  height: "0.45rem",
+  textAlign: "center",
+  lineHeight: "0.45rem",
+  fontSize: "0.16rem",
+  color: "#24272B",
+  borderBottom: "0.01rem solid #D2D2D7",
+  backgroundColor: "#F9F9F9",
+  fontWeight: 700,
+};
+
+function Header() {
+  return <div style={style}>我的</div>;
+}
+
+export default Header;
--- src/views/My/index.jsx
+++ src/views/My/index.jsx
@@ -1,8 +1,95 @@
 import React, { Component } from "react";
+import Header from "../../components/Header";
+import { List } from "antd-mobile";
+import style from "./index.module.scss";
+
+const Item = List.Item;
+const Brief = Item.Brief;
 
 class My extends Component {
   render() {
-    return <div> My </div>;
+    return (
+      <div className={style.my}>
+        <Header />
+        <List className={style.myList}>
+          <Item
+            className={style.author}
+            arrow="horizontal"
+            thumb="https://user-gold-cdn.xitu.io/2020/1/18/16fb901f1bac3975?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1"
+            multipleLine
+            onClick={() => {}}
+          >
+            <div className="name">橘猫很方</div> <Brief>前端开发工程师</Brief>
+          </Item>
+          {}
+          <Item
+            thumb={
+              <i
+                style={{ color: "#3274F6" }}
+                className="iconfont icon-xiaoxi"
+              ></i>
+            }
+          >
+            消息中心
+          </Item>
+          <Item
+            extra="180篇"
+            thumb={
+              <i
+                style={{ color: "#66C439" }}
+                className="iconfont icon-dianzan1"
+              ></i>
+            }
+          >
+            我赞过的
+          </Item>
+          <Item
+            extra="20个"
+            thumb={
+              <i
+                style={{ color: "#f6c55f" }}
+                className="iconfont icon-yanjing"
+              ></i>
+            }
+          >
+            收藏集
+          </Item>
+          <Item
+            extra="657篇"
+            thumb={
+              <i
+                style={{ color: "#abb3be" }}
+                className="iconfont icon-yanjing"
+              ></i>
+            }
+          >
+            阅读过的文章
+          </Item>
+          <Item
+            extra="20个"
+            thumb={
+              <i
+                style={{ color: "#acb4be" }}
+                className="iconfont icon-biaoqian"
+              ></i>
+            }
+          >
+            标签管理
+          </Item>
+          <Item
+            className="config"
+            thumb={
+              <i
+                style={{ color: "#acb4be" }}
+                className="iconfont icon-shezhi"
+              ></i>
+            }
+          >
+            设置
+          </Item>
+        </List>
+      </div>
+    );
   }
 }
 
